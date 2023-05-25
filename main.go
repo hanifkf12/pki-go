889545bdb2964b883b942b112efa328aef8ead17
FK5216 <hanif.fathansyah@privyid.tech>
2023-05-25 11:52:14 +0700

    fix sign data use pki

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
 	"crypto/x509"
-	"encoding/base64"
 	"encoding/pem"
 	"fmt"
 	"log"
@@ -33,8 +33,8 @@ func encrypt(data string) []byte {
 	return cipherText
 }
 
-func sign(data string) []byte {
-	bytes, err := os.ReadFile("public-new.key")
+func sign(data []byte) []byte {
+	bytes, err := os.ReadFile("private-new.key")
 	if err != nil {
 		return nil
 	}
@@ -42,16 +42,18 @@ func sign(data string) []byte {
 	if pubKeyBlock == nil {
 		log.Fatal("Failed to decode private key")
 	}
-	key, err := x509.ParsePKCS1PublicKey(pubKeyBlock.Bytes)
+	key, err := x509.ParsePKCS1PrivateKey(pubKeyBlock.Bytes)
 	if err != nil {
 		log.Fatalf("Failed to parse public key: %v", err)
 	}
+
+	hashMessage := sha256.Sum256(data)
 	//pubKey := key.(*rsa.PublicKey)
-	cipherText, err := rsa.EncryptPKCS1v15(rand.Reader, key, []byte(data))
+	signature, err := rsa.SignPKCS1v15(rand.Reader, key, crypto.SHA256, hashMessage[:])
 	if err != nil {
-		return nil
+		log.Fatal(err)
 	}
-	return cipherText
+	return signature
 }
 
 func decrypt(data []byte) []byte {
@@ -74,43 +76,47 @@ func decrypt(data []byte) []byte {
 	return oaep
 }
 
-func verify(data []byte) []byte {
-	bytes, err := os.ReadFile("private-new.key")
+func verify(data []byte, signature []byte) bool {
+	bytes, err := os.ReadFile("public-new.key")
 	if err != nil {
-		return nil
+		log.Println(err)
+		return false
 	}
 	privKeyBlock, _ := pem.Decode(bytes)
 	if privKeyBlock == nil {
 		log.Fatal("failed pem decode")
 	}
-	privateKey, err := x509.ParsePKCS1PrivateKey(privKeyBlock.Bytes)
+	pubKey, err := x509.ParsePKCS1PublicKey(privKeyBlock.Bytes)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("parse public kecy, ", err)
 	}
 
-	oaep, err := rsa.DecryptPKCS1v15(rand.Reader, privateKey, data)
+	hashedData := sha256.Sum256(data)
+	err = rsa.VerifyPKCS1v15(pubKey, crypto.SHA256, hashedData[:], signature)
 	if err != nil {
-		log.Fatal("sss", err)
+		log.Println(err)
+		return false
 	}
-	return oaep
+	return true
 }
 
 func main() {
-	encrypted := encrypt("hanif")
+
+	//sign data
+	signature := sign([]byte("halo"))
+	verified := verify([]byte("halo"), signature)
+	fmt.Println(verified)
+
+	//encryptdata
+	//encrypted := encrypt("hanif")
 	//os.WriteFile("test.txt", encrypted, 0666)
-
-	//signed := sign("halo")
-	//fmt.Println(string(signed))
-	//verified := verify(signed)
-	//fmt.Println(string(verified))
-
-	finalEncryption := base64.StdEncoding.EncodeToString(encrypted)
-	fmt.Println(finalEncryption)
-
-	decodeString, err := base64.StdEncoding.DecodeString(finalEncryption)
-	if err != nil {
-		log.Fatal(err)
-	}
-	decrypted := decrypt(decodeString)
-	fmt.Println(string(decrypted))
+	//finalEncryption := base64.StdEncoding.EncodeToString(encrypted)
+	//fmt.Println(finalEncryption)
+	//
+	//decodeString, err := base64.StdEncoding.DecodeString(finalEncryption)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//decrypted := decrypt(decodeString)
+	//fmt.Println(string(decrypted))
 }
